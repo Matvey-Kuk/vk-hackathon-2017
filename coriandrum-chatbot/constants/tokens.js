decadd044c2f46a583af474a2e2356df311daa87
yulia <yulia.termanvelyants@gmail.com>
2017-10-21 01:55:36 +0300

    get tokens

---
+++ coriandrum-chatbot/constants/tokens.js
@@ -0,0 +1,20 @@
+var makeRequest = require('request');
+
+var get = function (callback) {
+  makeRequest({
+    url: 'api.heroku.com/apps/coriandrum-chatbot/config-vars',
+    method: 'POST'
+  }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+
+        console.log("TOKENS RESPONSE", response);
+          callback();
+      } else {
+          console.log("TOKENS ERROR", error);
+      }
+  });
+};
+
+module.exports = {
+  get: get
+};
--- coriandrum-chatbot/index.js
+++ coriandrum-chatbot/index.js
@@ -3,59 +3,65 @@ var express = require('express');
 var app = express();
 var makeRequest = require('request');
 
-var CONFIRM_TOKEN = '4c029083';
-var ACCESS_TOKEN = '4553c596985b82bd80ed20da8283c38920a5d0a30b77200428763bf761d00f0952e70db7e894f126740ff';
 var MESSAGES_SEND_URL = 'https://api.vk.com/method/messages.send';
 
+var getToken = require('./constants/tokens').get;
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
+console.log(getToken);
 
-app.post('/', function(request, response){
-  console.log("REQUEST BODY:", request.body);
+var setApp = function (tokens) {
+  app.set('port', (process.env.PORT || 5000));
+  app.use(express.static(__dirname + '/public'));
+  app.use(bodyParser.json());
 
-  var body = request.body;
+  app.post('/', function(request, response){
+    console.log("REQUEST BODY:", request.body);
 
-  if (body.type === 'confirmation') {
-    response.send(CONFIRM_TOKEN);
-  } else if (body.type === 'message_new') {
-    var userId = body.object.user_id;
-    var text = body.object.body;
-    var reply;
+    var body = request.body;
 
-    switch (text) {
-      case '1':
-        console.log('1');
-        reply = '1111';
-        break;
-      default:
-        console.log('default');
-        reply = 'default';
+    if (body.type === 'confirmation') {
+      response.send(tokens.CONFIRM);
+    } else if (body.type === 'message_new') {
+      var userId = body.object.user_id;
+      var text = body.object.body;
+      var reply;
+
+      switch (text) {
+        case '1':
+          console.log('1');
+          reply = '1111';
+          break;
+        default:
+          console.log('default');
+          reply = 'default';
+      }
+
+      makeRequest({
+        url: MESSAGES_SEND_URL,
+        method: 'POST',
+        form: {
+          'message': reply,
+          'access_token': constants.ACCESS,
+          'user_id': userId
+        }
+      }, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              // Print out the response body
+              console.log("BODY", body);
+          } else {
+              console.log("ERROR", error);
+          }
+      });
+
+      response.send('ok');
     }
 
-    makeRequest({
-      url: MESSAGES_SEND_URL,
-      method: 'POST',
-      form: {
-        'message': reply,
-        'access_token': ACCESS_TOKEN,
-        'user_id': userId
-      }
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            // Print out the response body
-            console.log("BODY", body);
-        } else {
-            console.log("ERROR", error);
-        }
-    });
+  });
 
-    response.send('ok');
-  }
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+};
 
-});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+getToken(setApp);
