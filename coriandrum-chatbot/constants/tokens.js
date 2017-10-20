419aa1689e67ed80591fd51f4714b60d00f19de5
yulia <yulia.termanvelyants@gmail.com>
2017-10-21 02:01:43 +0300

    refactor tokens

--- coriandrum-chatbot/constants/tokens.js
+++ coriandrum-chatbot/constants/tokens.js
@@ -5,13 +5,12 @@ var get = function (callback) {
     url: 'https://api.heroku.com/apps/coriandrum-chatbot/config-vars',
     method: 'POST'
   }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-
-        console.log("TOKENS RESPONSE", response);
-          callback();
-      } else {
-          console.log("TOKENS ERROR", error);
-      }
+    if (!error && response.statusCode == 200) {
+      console.log("TOKENS RESPONSE", response);
+      callback();
+    } else {
+      console.log("TOKENS ERROR", error);
+    }
   });
 };
 
