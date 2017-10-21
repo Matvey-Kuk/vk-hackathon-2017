cc458ebea643eeb70cb3c04f961b2610e6fd0f76
yulia <yulia.termanvelyants@gmail.com>
2017-10-21 03:38:13 +0300

    debug

--- coriandrum-chatbot/helpers/tokens.js
+++ coriandrum-chatbot/helpers/tokens.js
@@ -2,7 +2,7 @@ var makeRequest = require('request');
 
 var requestDebug = require('request-debug');
 
-requestDebug({
+var get = requestDebug({
     url: 'https://api.heroku.com/apps/coriandrum-chatbot/config-vars',
     method: 'POST',
     headers: {
@@ -14,22 +14,22 @@ requestDebug({
     console.log("R", r);
 });
 
-var get = function (callback) {
-  makeRequest({
-    url: 'https://api.heroku.com/apps/coriandrum-chatbot/config-vars',
-    method: 'POST',
-    headers: {
-      'Accept': 'application/vnd.heroku+json; version=3'
-    }
-  }, function (error, response, body) {
-    if (!error) {
-     // console.log("TOKENS BODY", body);
-      callback();
-    } else {
-     // console.log("TOKENS ERROR", error);
-    }
-  });
-};
+// var get = function (callback) {
+//   makeRequest({
+//     url: 'https://api.heroku.com/apps/coriandrum-chatbot/config-vars',
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/vnd.heroku+json; version=3'
+//     }
+//   }, function (error, response, body) {
+//     if (!error) {
+//      // console.log("TOKENS BODY", body);
+//       callback();
+//     } else {
+//      // console.log("TOKENS ERROR", error);
+//     }
+//   });
+// };
 
 module.exports = {
   get: get
