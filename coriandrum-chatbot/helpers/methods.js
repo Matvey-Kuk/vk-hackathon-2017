b39bab901c0c801a1a944853d95e5f0d2ba200da
yulia <yulia.termanvelyants@gmail.com>
2017-10-21 21:32:26 +0300

    texts

--- coriandrum-chatbot/helpers/methods.js
+++ coriandrum-chatbot/helpers/methods.js
@@ -35,6 +35,20 @@ var findUser = function (userId, callback) {
   });
 };
 
+var replyToChat = function (reply, userId, callback) {
+  makeRequest({
+    url: urls.VK_MESSAGES_SEND,
+    method: 'POST',
+    form: {
+      'message': reply,
+      'access_token': process.env.ACCESS_TOKEN,
+      'user_id': userId
+    }
+  }, function (error, response, body) {
+    callback(error, response, body);
+  });
+};
+
 var handleNewMessage = function (object) {
     var userId = object.user_id;
     var text = object.body;
@@ -52,15 +66,7 @@ var handleNewMessage = function (object) {
     }
 
     // reply to chat
-    makeRequest({
-      url: urls.VK_MESSAGES_SEND,
-      method: 'POST',
-      form: {
-        'message': reply,
-        'access_token': process.env.ACCESS_TOKEN,
-        'user_id': userId
-      }
-    }, function (error, response, body) {
+    replyToChat(reply, userId, function (error, response, body) {
       console.log(body);
       if (!error && response.statusCode == 200) {
 
