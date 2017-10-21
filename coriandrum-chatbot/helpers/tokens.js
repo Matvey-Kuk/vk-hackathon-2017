257f7be741ad972a05ced8cd9e8a9384ed024ddb
yulia <yulia.termanvelyants@gmail.com>
2017-10-21 04:10:21 +0300

    token

--- coriandrum-chatbot/helpers/tokens.js
+++ coriandrum-chatbot/helpers/tokens.js
@@ -4,13 +4,15 @@ var requestDebug = require('request-debug');
 
 requestDebug(makeRequest);
 
+console.log(process.env);
+
 var get = function (callback) {
   makeRequest({
     url: 'https://api.heroku.com/apps/coriandrum-chatbot/config-vars',
     method: 'GET',
     headers: {
       'Accept': 'application/vnd.heroku+json; version=3',
-      'Authorization': 'Basic c2h1dHVwNG5kc2s0dGVAZ21haWwuY29tOjMxZmQ3NDk3LTE2YzYtNDBlNS05MTQyLWVhZDc5MjViM2E5MQ=='
+      'Authorization': 'Basic '
     }
   }, function (error, response, body) {
     console.log(error);
