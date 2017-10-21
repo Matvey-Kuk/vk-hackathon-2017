594fd774764b47e1c30f8836b203a8f60d324fd3
yulia <yulia.termanvelyants@gmail.com>
2017-10-21 03:30:45 +0300

    require debug

--- coriandrum-chatbot/helpers/tokens.js
+++ coriandrum-chatbot/helpers/tokens.js
@@ -1,5 +1,19 @@
 var makeRequest = require('request');
 
+var requestDebug = require('request-debug');
+
+requestDebug({
+    url: 'https://api.heroku.com/apps/coriandrum-chatbot/config-vars',
+    method: 'POST',
+    headers: {
+      'Accept': 'application/vnd.heroku+json; version=3'
+    }
+  }, function(type, data, r) {
+    console.log("TYPE", type);
+    console.log("DATA,", data);
+    console.log("R", r);
+});
+
 var get = function (callback) {
   makeRequest({
     url: 'https://api.heroku.com/apps/coriandrum-chatbot/config-vars',
@@ -9,10 +23,10 @@ var get = function (callback) {
     }
   }, function (error, response, body) {
     if (!error) {
-      console.log("TOKENS BODY", body);
+     // console.log("TOKENS BODY", body);
       callback();
     } else {
-      console.log("TOKENS ERROR", error);
+     // console.log("TOKENS ERROR", error);
     }
   });
 };
