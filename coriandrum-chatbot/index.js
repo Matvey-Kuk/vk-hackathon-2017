3ff0fd807fc6ad6ce405546cc0289950e8f87624
yulia <yulia.termanvelyants@gmail.com>
2017-10-22 02:14:15 +0300

    deploy

--- coriandrum-chatbot/index.js
+++ coriandrum-chatbot/index.js
@@ -11,7 +11,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
-app.post('/', function(request, response) {
+app.post('/', function (request, response) {
   var body = request.body;
 
   if (body.type === 'confirmation') {
@@ -22,7 +22,11 @@ app.post('/', function(request, response) {
   }
 });
 
-app.listen(app.get('port'), function() {
+app.post('/update', function (request, response) {
+  console.log(request, response);
+});
+
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
