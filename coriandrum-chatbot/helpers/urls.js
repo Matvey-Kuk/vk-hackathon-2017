9c1487e5ec09ebf821f244f36f0305bdca84b6e8
yulia <yulia.termanvelyants@gmail.com>
2017-10-21 13:23:46 +0300

    deploy

---
+++ coriandrum-chatbot/helpers/urls.js
@@ -0,0 +1,8 @@
+var VK_API = 'https://api.vk.com/method/';
+var CRNDRM_API = 'http://46.101.172.191/api/';
+
+module.exports = {
+  VK_MESSAGES_SEND: VK_API + 'messages.send',
+  CRNDRM_USERS: CRNDRM_API + 'users'
+};
+
