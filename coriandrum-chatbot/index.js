fd1f9c54afe1f2c45ebb3cd1aaa575a84a24a604
yulia <yulia.termanvelyants@gmail.com>
2017-10-21 13:17:28 +0300

    deploy

--- coriandrum-chatbot/index.js
+++ coriandrum-chatbot/index.js
@@ -2,71 +2,63 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var makeRequest = require('request');
-
-
 var requestDebug = require('request-debug');
+var URLS = require('../helpers/urls');
 
 requestDebug(makeRequest);
 
-var MESSAGES_SEND_URL = 'https://api.vk.com/method/messages.send';
+//var MESSAGES_SEND_URL = 'https://api.vk.com/method/messages.send';
 
-//var getToken = require('./helpers/tokens').get;
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-console.log("PROCESS.ENV", process.env);
+app.post('/', function(request, response){
+  ///console.log("REQUEST BODY:", request.body);
 
-//var setApp = function (tokens) {
-  app.set('port', (process.env.PORT || 5000));
-  app.use(express.static(__dirname + '/public'));
-  app.use(bodyParser.json());
+  var body = request.body;
 
-  app.post('/', function(request, response){
-    console.log("REQUEST BODY:", request.body);
+  if (body.type === 'confirmation') {
+    response.send(process.env.CONFIRM_TOKEN);
+  } else if (body.type === 'message_new') {
+    var userId = body.object.user_id;
+    var text = body.object.body;
+    var reply;
 
-    var body = request.body;
-
-    if (body.type === 'confirmation') {
-      response.send(process.env.CONFIRM_TOKEN);
-    } else if (body.type === 'message_new') {
-      var userId = body.object.user_id;
-      var text = body.object.body;
-      var reply;
-
-      switch (text) {
-        case '1':
-          console.log('1');
-          reply = '1111';
-          break;
-        default:
-          console.log('default');
-          reply = 'default';
-      }
-
-      makeRequest({
-        url: MESSAGES_SEND_URL,
-        method: 'POST',
-        form: {
-          'message': reply,
-          'access_token': process.env.ACCESS_TOKEN,
-          'user_id': userId
-        }
-      }, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-              // Print out the response body
-              console.log("BODY", body);
-          } else {
-              console.log("ERROR", error);
-          }
-      });
-
-      response.send('ok');
+    switch (text) {
+      case '1':
+        console.log('1');
+        reply = '1111';
+        break;
+      default:
+        console.log('default');
+        reply = 'default';
     }
 
-  });
+    makeRequest({
+      url: URLS.VK_MESSAGES_SEND,
+      method: 'POST',
+      form: {
+        'message': reply,
+        'access_token': process.env.ACCESS_TOKEN,
+        'user_id': userId
+      }
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+            console.log("BODY", body);
+        } else {
+            console.log("ERROR", error);
+        }
+    });
 
-  app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-  });
-// };
+    response.send('ok');
+  }
+
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
-// getToken(setApp);
