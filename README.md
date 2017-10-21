f27ccdc71015e0c5c2f5b173799c1553c6d04b1d
motakuk <motakuk@gmail.com>
2017-10-21 04:19:48 +0300

    Static files server

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Heroku: `git subtree push --prefix coriandrum-chatbot heroku master`
+
+Deploy: 
+```
+docker build -t motakukk/vk-hack-2017:latest .
+docker push motakukk/vk-hack-2017:latest
+```
+
+On server:
+
+```
+docker pull motakukk/vk-hack-2017:latest && docker service update --image motakukk/vk-hack-2017:latest bck
+```
--- coriandrum/coriandrum/urls.py
+++ coriandrum/coriandrum/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('apps.api.urls')),
     url(r'^', include('apps.public_rating.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
