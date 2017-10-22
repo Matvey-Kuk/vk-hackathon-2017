77b751e39cb39f2446514dc8c0c28d154313915f
yulia <yulia.termanvelyants@gmail.com>
2017-10-22 09:26:57 +0300

    share

--- coriandrum/static/script.js
+++ coriandrum/static/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     btn.on('click', function () {
       VK.api("wall.post", {
-        "message": "Hello!"
+        "message": "Я уважаемый человек в Лентаче https://vk.com/app6228819"
       }, function (data) {
         console.log("Post ID:" + data.response.post_id);
       });
