d5ec06b785eb3d65f1f690de45934d163572aa3f
yulia <yulia.termanvelyants@gmail.com>
2017-10-22 09:14:26 +0300

    share on btn

--- coriandrum/static/script.js
+++ coriandrum/static/script.js
@@ -1,6 +1,18 @@
-VK.init(function() { 
-  VK.api("wall.post", {"message": "Hello!"}, function (data) {
-    console.log("Post ID:" + data.response.post_id);
-  });
-}, function() {
-}, '5.68')
+$(document).ready(function () {
+  var btn = $("#share-btn");
+
+  VK.init(function() { 
+
+    btn.on('click', function () {
+      VK.api("wall.post", {
+        "message": "Hello!"
+      }, function (data) {
+        console.log("Post ID:" + data.response.post_id);
+      });
+    });
+
+  }, function() {
+
+  }, '5.68')
+});
+
