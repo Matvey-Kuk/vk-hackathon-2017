ccedec8985dddc4509c803ee2be46f12a9aa153c
kolya <n.kogan@cdnvideo.ru>
2017-10-22 08:10:19 +0300

    Add admin button to contributor

--- coriandrum/apps/public_rating/views.py
+++ coriandrum/apps/public_rating/views.py
@@ -11,16 +11,18 @@ def leaderboard(request):
         context = {"all_users": all_users}
         return render(request, "public_rating/leaderboard.html", context)
 
+
 def entry(request):
     if request.method == "GET":
         vk_user_id = request.GET.get("viewer_id", "")
         viewer_type = int(request.GET.get("viewer_type", "0"))
         if viewer_type in [3, 4]:  # user is a community editor/admin
-            return HttpResponseRedirect("/god/")
+            return HttpResponseRedirect("/contributor/" + vk_user_id + "/admin")
         else: # user is not a member/is a regular member/is a moderator
             return HttpResponseRedirect("/contributor/" + vk_user_id)
 
-def contributor(request, user_id):
+
+def contributor(request, user_id, is_admin=False):
     if request.method == "GET":
         user_qs = CoriandrumUser.objects.filter(vk_user_id=user_id)
         if not user_qs:
