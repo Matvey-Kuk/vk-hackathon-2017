79d178eaa88e459e540541beb311b800ecb41476
yulia <yulia.termanvelyants@gmail.com>
2017-10-22 07:34:58 +0300

    cache

--- coriandrum/static/script.js
+++ coriandrum/static/script.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
   var $shareBtn = $("#share-btn");
-  $shareBtn.attr('href', 'http://vk.com/share.php?url=' + window.location.href);
+  $shareBtn.attr('href', 'https://vk.com/share.php?url=' + window.location.href);
 });
